5eb23b89204cf32d5bf66e9f4da7c274ff7845d8
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2023-12-26 15:41:39 -0500

    Implement Events

---
+++ FaceFacts/EditEventView.swift
@@ -0,0 +1,18 @@
+//
+//  EditEventView.swift
+//  FaceFacts
+//
+//  Created by Parth Antala on 2023-12-26.
+//
+
+import SwiftUI
+
+struct EditEventView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditEventView()
+}
---
+++ FaceFacts/Event.swift
@@ -0,0 +1,8 @@
+//
+//  Event.swift
+//  FaceFacts
+//
+//  Created by Parth Antala on 2023-12-26.
+//
+
+import Foundation
