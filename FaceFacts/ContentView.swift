d73fa135d79f330daafb4fca511beaed3ae11215
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2023-12-26 14:01:27 -0500

    Initial Commit

---
+++ FaceFacts/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FaceFacts
+//
+//  Created by Parth Antala on 2023-12-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FaceFacts/FaceFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceFactsApp.swift
+//  FaceFacts
+//
+//  Created by Parth Antala on 2023-12-26.
+//
+
+import SwiftUI
+
+@main
+struct FaceFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
